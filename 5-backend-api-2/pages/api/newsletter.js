3b531e47641d4877a8a3d6cb4cdd7f1e5db9d530
AHMAD-KHATIB <ahmadkhatib923@gmail.com>
2023-06-17 23:42:08 +0300

    using mongodb for comments

--- 5-backend-api-2/pages/api/newsletter.js
+++ 5-backend-api-2/pages/api/newsletter.js
@@ -10,14 +10,14 @@ async function handler(req, res) {
     }
 
     const client = await MongoClient.connect(
-      'mongodb+srv://db-uri.mongodb.net/newsletter?retryWrites=true&w=majority'
+      'mongodb+srv://maximilian:8ZO3ycZqJ23kWBQx@cluster0.ntrwp.mongodb.net/events?retryWrites=true&w=majority'
     );
     const db = client.db();
 
-    await db.collection('emails').insertOne({ email: userEmail });
+    await db.collection('newsletter').insertOne({ email: userEmail });
 
     client.close();
-
+  
     res.status(201).json({ message: 'Signed up!' });
   }
 }
